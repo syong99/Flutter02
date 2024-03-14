9d70487ab9ab0fcaf6bc6fb1cf94268c16d02ec6
syong99 <sy99639@naver.com>
2024-03-14 10:49:37 +0900

    MetarialApp,Scaffold 구조

--- chap01_flutter_basic/lib/main.dart
+++ chap01_flutter_basic/lib/main.dart
@@ -9,6 +9,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'App타이틀',
+        //App의 전반적인 테마를 설정한다.
+        theme: ThemeData(
+          //텍스트의 style을 미리 설정하고 호출해 사용 가능하다.
+          textTheme: TextTheme(
+            bodyText1: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Appbar'),
+            backgroundColor: Colors.blue,
+          ),
+          body: Center(
+            child: Text('iphone 22'),
+          ),
+          // BottomNavigationBar
+          bottomNavigationBar: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'business',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                label: 'school',
+              ),
+            ],
+          ),
+          // FloatingActionButton
+          floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.access_alarm),
+              onPressed: () => {
+                    print('hello'),
+                  }),
+        ));
   }
 }
